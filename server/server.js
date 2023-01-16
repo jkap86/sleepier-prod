20d25b2120c49c5134a3b0a588a4bfe57982308e
jkap86 <jkap86@gmail.com>
2023-01-16 17:11:04 -0500

    change server start from http to app.listen

--- server/server.js
+++ server/server.js
@@ -209,6 +209,6 @@ app.get('*', async (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000;
-http.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
